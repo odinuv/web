cb1dbcc9321e825ffc9f95d028b5b3d7d25b65e2
Odin <odinuv@gmail.com>
2016-08-24 00:12:38 +0200

    fix: break on first failure

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@
 gitLastCommit=$(git show --summary --grep="Merge pull request")
 if [[ -z "$gitLastCommit" ]]
 then
-	lastCommit=$(git log --format="%H" -n 1)
+    lastCommit=$(git log --format="%H" -n 1)
 else
-	echo "We got a Merge Request!"
-	#take the last commit and take break every word into an array
-	arr=($gitLastCommit)
-	#the 5th element in the array is the commit ID we need. If git log changes, this breaks. :(
-	lastCommit=${arr[4]}
+    echo "We got a Merge Request!"
+    #take the last commit and take break every word into an array
+    arr=($gitLastCommit)
+    #the 5th element in the array is the commit ID we need. If git log changes, this breaks. :(
+    lastCommit=${arr[4]}
 fi
 echo $lastCommit
 
@@ -18,12 +18,19 @@ if [ ${#filesChanged[@]} -eq 0 ]; then
     echo "No files to update"
 else
     for f in $filesChanged
-	do
-		#do not upload these files that aren't necessary to the site
-		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
-		then
-	 		echo "Uploading $f"
-	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS $FTP_TARGET/$f
-		fi
-	done
+    do
+        #do not upload these files that aren't necessary to the site
+        if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
+        then
+            echo "Uploading $f"
+            curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS $FTP_TARGET/$f
+            if [ $? -eq 0 ]
+            then
+              echo "Successfully uploade file"
+            else
+              echo "Could not upload file" >&2
+              exit 1
+            fi
+        fi
+    done
 fi
