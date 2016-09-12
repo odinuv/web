ba002e96b7801a48e0a5dbe7f72117725385b59f
Jiri Lysek <jiri.lysek@mendelu.cz>
2016-09-12 14:13:41 +0200

    fix code indentation

--- _includes/en/apv/walkthrough/dynamic-page/ifelse-3.php
+++ _includes/en/apv/walkthrough/dynamic-page/ifelse-3.php
@@ -4,27 +4,27 @@ $numberOfWheels = 4;
 $drive = 'electric';
 
 switch ($numberOfWheels) {
-	case 4:
-		switch ($drive) {
-			case 'electric':
-			    echo "It's an electric car.";
-			    break;
-			case 'combustion':
-    			echo "It's a smoking car.";
-    			break;
-			case 'feet':
-			    echo "It's a Flintstones car.";
-			    break;
-			default:
-				echo "It's an unknown car.";
-		}
-		break;
-	case 2:
-	    echo "It's a bike.";
-	    break;
-	case 1:
-	    echo "It's a unicycle.";
-	    break;
-	default:
-    	echo "I don't know what that is.";
+    case 4:
+        switch ($drive) {
+            case 'electric':
+                echo "It's an electric car.";
+                break;
+            case 'combustion':
+                echo "It's a smoking car.";
+                break;
+            case 'feet':
+                echo "It's a Flintstones car.";
+                break;
+            default:
+                echo "It's an unknown car.";
+        }
+        break;
+    case 2:
+        echo "It's a bike.";
+        break;
+    case 1:
+        echo "It's a unicycle.";
+        break;
+    default:
+        echo "I don't know what that is.";
 }
