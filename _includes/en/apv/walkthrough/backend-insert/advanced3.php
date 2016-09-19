8dc560c076d243cbd8872d09abfd664e6879056c
Jiri Lysek <jiri.lysek@mendelu.cz>
2016-09-19 13:48:01 +0200

    fix examples

--- _includes/en/apv/walkthrough/backend-insert/advanced3.php
+++ _includes/en/apv/walkthrough/backend-insert/advanced3.php
@@ -6,11 +6,13 @@ try {
     $city = "Bucharest";
     $stmt1 = $db->prepare("INSERT INTO location (city) VALUES (:c)");
     $stmt1->bindValue(":c", $city);
+    $stmt1->execute();
 
     $addressID = $db->lastInsertId("location_id_location_seq");
 
     $stmt2 = $db->prepare("INSERT INTO person (..., address_id, ...) VALUES (..., :aid, ...)");
     $stmt2->bindValue(":aid", $addressID);
+    $stmt2->execute();
 
     $db->commit();  //if we got to this line, both rows are inserted, we can finish the transaction
 } catch(PDOException $e) {
