73e93c36a96f986626d62f666e2f590e478fe215
Odin <odinuv@gmail.com>
2016-09-21 12:54:00 +0200

    lang corrections

--- _includes/en/apv/walkthrough/backend-insert/advanced2.php
+++ _includes/en/apv/walkthrough/backend-insert/advanced2.php
@@ -8,7 +8,11 @@ try {
 
     $addressID = $db->lastInsertId("location_id_location_seq");
 
-    $stmt2 = $db->prepare("INSERT INTO person (..., address_id, ...) VALUES (..., :aid, ...)");
+    $stmt2 = $db->prepare("INSERT INTO person (first_name, last_name, nickname, address_id) 
+    	VALUES (:first_name, :last_name, :nickname, :aid)");
+    $stmt2->bindValue(":first_name", 'Janice');
+    $stmt2->bindValue(":last_name", 'Doe');
+    $stmt2->bindValue(":nickname", 'JD');
     $stmt2->bindValue(":aid", $addressID);
     $stmt2->execute();
 } catch(PDOException $e) {
