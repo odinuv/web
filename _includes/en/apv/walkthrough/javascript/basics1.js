8fdd675e94957681f42533dcc11b6d933f391a83
Jiri Lysek <jiri.lysek@mendelu.cz>
2016-12-06 14:43:48 +0100

    remove blank lines from source code examples

--- _includes/en/apv/walkthrough/javascript/basics1.js
+++ _includes/en/apv/walkthrough/javascript/basics1.js
@@ -3,17 +3,14 @@ var stringVariable = "Hello world";
 var numberVariable = 1.2345;
 var boolVariable = false;
 console.log(stringVariable, numberVariable, boolVariable);
-
 /* arrays */
 var arrayVariable = [1,2,3,4,'This is an array', false, true, [10,20,30]];
 console.log(arrayVariable);
-
 /* reference to a function */
 var functionVariable = function(param) {
     alert(param);
 };
 functionVariable("Call a function");
-
 /* object */
 var objectVariable = {
     key: "value",
@@ -25,5 +22,4 @@ var objectVariable = {
 };
 console.log(objectVariable);
 console.log(objectVariable.number);
-
 objectVariable.method("Calling a method of object.");
