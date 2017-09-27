e8812d79444f33b1efa7ef3d13132a236e91d7dc
Odin <odinuv@gmail.com>
2017-09-27 13:19:55 +0200

    fix: path

--- _docker/mdeploy-run.sh
+++ _docker/mdeploy-run.sh
@@ -2,8 +2,9 @@
 set -e
 
 cd $TRAVIS_BUILD_DIR
-$TRAVIS_BUILD_DIR/_docker/replace.sh
+./_docker/replace.sh
 
+cd ./_docker
 docker-compose run --rm -e JEKYLL_ENV=production jekyll jekyll build
 
 docker-compose run --rm deploy
