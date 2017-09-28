aad4a79b9d5c32e071affd7f2b08e65766eb4880
odinuv <odinuv@gmail.com>
2017-09-28 09:10:21 +0200

    feat: use different GAs fix: favicon and some meta

--- _docker/mdeploy-run.sh
+++ _docker/mdeploy-run.sh
@@ -3,6 +3,7 @@ set -e
 
 cd $TRAVIS_BUILD_DIR
 ./_docker/replace.sh
+cp -f ./_includes/analytics-x.html ./_includes/analytics.html
 
 cd ./_docker
 docker-compose run --rm -e JEKYLL_ENV=production jekyll jekyll build
