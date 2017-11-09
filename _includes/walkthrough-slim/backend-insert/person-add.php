710fd738fde17bf4af0c534139968601396a7091
Jiří Lýsek <jiri.lysek@mendelu.cz>
2017-11-09 11:11:45 +0100

    message variable not mandatory

--- _includes/walkthrough-slim/backend-insert/person-add.php
+++ _includes/walkthrough-slim/backend-insert/person-add.php
@@ -1,6 +1,5 @@
 <?php
 
 $app->get('/add-person', function(Request $request, Response $response, $args) {
-    $tplVars['message'] = '';
-    $this->view->render($response, 'templates/person-add.latte', $tplVars);
+    $this->view->render($response, 'person-add.latte');
 });
