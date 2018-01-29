d515034a945fb5ca9c70c2434ecda5beb13147e3
Jiří Lýsek <jiri.lysek@mendelu.cz>
2018-01-29 16:56:02 +0100

    deploy to a subfolder

--- _docker/replace.sh
+++ _docker/replace.sh
@@ -7,4 +7,4 @@ echo "Replacing permalinks"
 grep -rl 'permalink: /' . | xargs sed -i 's@permalink: /@permalink: /~'$MFTP_USER'/@g'
 echo "Replacing menulinks"
 grep -rl 'url: /' ./_data/ | xargs sed -i 's@url: /@url: /~'$MFTP_USER'/@g'
-grep -rl '#baseurl: "/~xpopelka/"' ./_config.yml | xargs sed -i 's@#baseurl: "/~'$MFTP_USER'/"@baseurl: "/~'$MFTP_USER'/"@g'
+grep -rl '#baseurl: "/~xpopelka/"' ./_config.yml | xargs sed -i 's@#baseurl: "/~'$MFTP_USER'/"@baseurl: "/~'$MFTP_USER'/'$MFTP_PATH'"@g'
