b86b9c361dae5ca166fe3906f41d3862546a994c
Jiří Lýsek <jiri.lysek@mendelu.cz>
2018-01-31 14:33:11 +0100

    use path for all links

--- _docker/replace.sh
+++ _docker/replace.sh
@@ -2,9 +2,9 @@
 set -e
 
 echo "Replacing links"
-grep -rlF '](/' . | xargs sed -i 's@](/@](/~'$MFTP_USER'/@g'
+grep -rlF '](/' . | xargs sed -i 's@](/@](/~'$MFTP_USER'/'$MFTP_PATH'/@g'
 echo "Replacing permalinks"
-grep -rl 'permalink: /' . | xargs sed -i 's@permalink: /@permalink: /~'$MFTP_USER'/@g'
+grep -rl 'permalink: /' . | xargs sed -i 's@permalink: /@permalink: /~'$MFTP_USER'/'$MFTP_PATH'/@g'
 echo "Replacing menulinks"
-grep -rl 'url: /' ./_data/ | xargs sed -i 's@url: /@url: /~'$MFTP_USER'/@g'
+grep -rl 'url: /' ./_data/ | xargs sed -i 's@url: /@url: /~'$MFTP_USER'/'$MFTP_PATH'/@g'
 grep -rl '#baseurl: "/~xpopelka/"' ./_config.yml | xargs sed -i 's@#baseurl: "/~xpopelka/"@baseurl: "/~'$MFTP_USER'/'$MFTP_PATH'"@g'
