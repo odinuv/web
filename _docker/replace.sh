c4905fd5e21f717ee8ad4058854ef91d4431bbbf
Jiří Lýsek <jiri.lysek@mendelu.cz>
2018-02-08 09:48:42 +0100

    replace slides back link

--- _docker/replace.sh
+++ _docker/replace.sh
@@ -8,3 +8,5 @@ grep -rl 'permalink: /' . | xargs sed -i 's@permalink: /@permalink: /~'$MFTP_USE
 echo "Replacing menulinks"
 grep -rl 'url: /' ./_data/ | xargs sed -i 's@url: /@url: /~'$MFTP_USER'/'$MFTP_PATH'/@g'
 grep -rl '#baseurl: "/~xpopelka/"' ./_config.yml | xargs sed -i 's@#baseurl: "/~xpopelka/"@baseurl: "/~'$MFTP_USER'/'$MFTP_PATH'/"@g'
+echo "Replacing slides link"
+sed -i "s@<a href='/slides/'@<a href='"$MFTP_USER"/"$MFTP_PATH"/slides/'@g"
