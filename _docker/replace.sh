66fdef4aeacee243ee5af450ad0de9c2a82f4986
Jiří Lýsek <jiri.lysek@mendelu.cz>
2018-02-08 14:55:42 +0100

    relative path for slides back button

--- _docker/replace.sh
+++ _docker/replace.sh
@@ -9,4 +9,3 @@ echo "Replacing menulinks"
 grep -rl 'url: /' ./_data/ | xargs sed -i 's@url: /@url: /~'$MFTP_USER'/'$MFTP_PATH'/@g'
 grep -rl '#baseurl: "/~xpopelka/"' ./_config.yml | xargs sed -i 's@#baseurl: "/~xpopelka/"@baseurl: "/~'$MFTP_USER'/'$MFTP_PATH'/"@g'
 echo "Replacing slides link"
-sed -i "s@<a href='/slides/'@<a href='"$MFTP_USER"/"$MFTP_PATH"/slides/'@g"
