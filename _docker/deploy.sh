e2cd14342deaf2855f0e37499bc176c25f42ee83
Odin <odinuv@gmail.com>
2018-03-09 14:49:29 +0100

    fix: escape variables

--- _docker/deploy.sh
+++ _docker/deploy.sh
@@ -8,24 +8,24 @@ then
 else
     echo "We got a Merge Request!"
     #take the last commit and take break every word into an array
-    arr=($gitLastCommit)
+    arr=(${gitLastCommit})
     #the 5th element in the array is the commit ID we need. If git log changes, this breaks. :(
     lastCommit=${arr[4]}
 fi
-echo $lastCommit
+echo ${lastCommit}
 
 cd ../_site
 filesChanged=$(find . -type f)
 if [ ${#filesChanged[@]} -eq 0 ]; then
     echo "No files to update"
 else
-    for f in $filesChanged
+    for f in ${filesChanged}
     do
         #do not upload these files that aren't necessary to the site
         if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
         then
             echo "Uploading $f"
-            curl -s --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS $FTP_TARGET/html/$f
+            curl -s --ftp-create-dirs -T ${f} -u ${FTP_USER}:${FTP_PASS} ${FTP_TARGET}/html/${f}
             if [ $? -ne 0 ]; then
               echo "Could not upload file" >&2
               exit 1
