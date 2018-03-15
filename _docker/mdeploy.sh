113dd7e1cb32d417eb7fc5827658d9a7084d9321
Jiří Lýsek <jiri.lysek@mendelu.cz>
2018-03-15 09:23:52 +0100

    upload using lftp

--- _docker/mdeploy.sh
+++ _docker/mdeploy.sh
@@ -9,17 +9,28 @@ filesChanged=$(find . -type f)
 if [ ${#filesChanged[@]} -eq 0 ]; then
     echo "No files to update"
 else
+    COMMAND="set sftp:auto-confirm yes
+open -u $MFTP_USER,$MFTP_PASS $MFTP_TARGET"
     for f in $filesChanged
     do
         #do not upload these files that aren't necessary to the site
         if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
         then
-            echo "Uploading $f"
-            curl -s --insecure --ftp-create-dirs -T $f -u $MFTP_USER:$MFTP_PASS $MFTP_TARGET/$f
-            if [ $? -ne 0 ]; then
-              echo "Could not upload file" >&2
-              exit 1
-            fi
+            COMMAND="$COMMAND
+put $f"
+            #echo "Uploading $f"
+            #curl -s --insecure --ftp-create-dirs -T $f -u $MFTP_USER:$MFTP_PASS $MFTP_TARGET/$f
+            #if [ $? -ne 0 ]; then
+              #echo "Could not upload file" >&2
+              #exit 1
+            #fi
         fi
     done
+    COMMAND="$COMMAND
+close
+exit"
+    lftp -c "$COMMAND"
+    if [ $? -ne 0 ]; then
+        echo "File upload failed" >&2
+    fi
 fi
