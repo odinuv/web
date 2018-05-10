40f8bc8c58f1f25c9007fbcf71e8598de27fb97f
Jiri Lysek <jlx@seznam.cz>
2018-05-10 22:37:46 +0200

    more debug messages

--- docker/mdeploy-run.sh
+++ docker/mdeploy-run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 
+echo "Starting mdeploy-run"
+
 cd $TRAVIS_BUILD_DIR
 ./docker/replace.sh
 cp -f ./_includes/analytics-x.html ./_includes/analytics.html
+
+echo "Executing docker"
 
 cd ./docker
 docker-compose run --rm -e JEKYLL_ENV=production jekyll jekyll build
