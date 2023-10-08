d7558f637ea2a92042a7adb44033cdfe9226c898
Ondrej Popelka <odinuv@gmail.com>
2023-10-08 10:17:09 +0200

    remove planuml Fixes #43

--- provisioning/deploy.sh
+++ provisioning/deploy.sh
@@ -4,7 +4,6 @@ set -Eeuo pipefail
 COMMAND="set sftp:auto-confirm yes
 open -u $FTP_USER,$FTP_PASS $FTP_TARGET
 mirror --reverse --overwrite --parallel=4 -v /code/_site/ /html/
-close
 exit"
 
 lftp -c "$COMMAND"
