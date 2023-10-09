5c010b38cd89a18fb23c7da1bd391442e5771ab9
Ondrej Popelka <odinuv@gmail.com>
2023-10-09 20:51:29 +0200

    optimize upload, fixes #45

--- provisioning/deploy.sh
+++ provisioning/deploy.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-COMMAND="set sftp:auto-confirm yes
-open -u $FTP_USER,$FTP_PASS $FTP_TARGET
-mirror --reverse --overwrite --parallel=4 -v /code/_site/ /html/
-exit"
+DIR_TO_UPLOAD='/code/_site/'
+EXCLUDE_FILE='/code/provisioning/legacy_files.txt'
+TMP_FILE='/tmp/upload_files.txt'
+CMD_FILE='/tmp/commands.txt'
+if [ "$1" == "full" ]; then
+    find "$DIR_TO_UPLOAD" -type f > "$TMP_FILE"
+else
+    find "$DIR_TO_UPLOAD" -type f | grep -v -f "$EXCLUDE_FILE" > "$TMP_FILE"
+fi
 
-lftp -c "$COMMAND"
+printf "pwd \n" > "$CMD_FILE"
+while IFS= read -r line
+do
+    printf "put %s /html/%s\n" "$line" "$(realpath --relative-to=$DIR_TO_UPLOAD "$line")" >> "$CMD_FILE"
+done < "$TMP_FILE"
+
+printf "quit \n" >> "$CMD_FILE"
+
+# cat "$CMD_FILE"
+export SSHPASS=$FTP_PASS
+sshpass -e sftp -oBatchMode=no -v -b "$CMD_FILE" "$FTP_USER@$FTP_TARGET"
+
+rm "$CMD_FILE"
+rm "$TMP_FILE"
